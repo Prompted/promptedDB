96a723581a85e21550101a71e9edbf0c068557cd
Jason Jordan <jtjordan13@gmail.com>
2017-04-15 16:15:09 -0400

    I believe I have sign in and sign up working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
-            :recoverable, :rememberable, :trackable, :validatable,
-            :confirmable, :omniauthable
+            :recoverable, :rememberable, :trackable #:validatable,
+            #:confirmable, :omniauthable
     include DeviseTokenAuth::Concerns::User
 
 end
--- lib/assets/testloging.rb
+++ lib/assets/testloging.rb
@@ -1,8 +1,20 @@
 require "HTTParty"
+require 'pry'
 
-resp = HTTParty.post("http://prompted-db.herokuapp.com/auth",
-    :query => { :email => "jtjordan13@gmail.com",
+resp = HTTParty.post("http://localhost:3000/auth",
+    :query => { :email => "jtjordan16@gmail.com",
                 :password => "hunter2",
-                :password_confirmation => "hunter2"})
+                :password_confirmation => "hunter2",
+                :confirm_success_url => 'http://localhost:3000'})
 
 puts resp.code
+
+binding.pry
+
+resp2 = HTTParty.post("http://localhost:3000/auth/sign_in",
+    :query => { :email => "jtjordan16@gmail.com",
+                :password => "hunter2",
+                :password_confirmation => "hunter2",
+                :confirm_success_url => 'http://localhost:3000'})
+
+binding.pry
