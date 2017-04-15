b44df7231936894d6e056bbd1bf7c105d752399b
Jason Jordan <jtjordan13@gmail.com>
2017-04-15 15:47:39 -0400

    Trying something else

--- lib/assets/testloging.rb
+++ lib/assets/testloging.rb
@@ -1,6 +1,8 @@
 require "HTTParty"
 
-HTTParty.post("http://prompted-db.herokuapp.com/auth",
+resp = HTTParty.post("http://prompted-db.herokuapp.com/auth",
     :query => { :email => "jtjordan13@gmail.com",
                 :password => "hunter2",
                 :password_confirmation => "hunter2"})
+
+puts resp.code
