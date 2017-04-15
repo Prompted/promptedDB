02f3b8112544d0824a5bbd1471f1035cd38c88f4
Jason Jordan <jtjordan13@gmail.com>
2017-04-15 15:17:48 -0400

    working on getting api to talk with front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
+  #include DeviseTokenAuth::Concerns::SetUserByToken
+  #protect_from_forgery with: :null_session
+
   protect_from_forgery with: :null_session
+
+before_filter :configure_permitted_parameters, if: :devise_controller?
+
+# before_action :set_format
+# before_action :authenticate_user!
+
+def set_format
+  request.format = :json
+end
+
+protected
+
+ def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:screen_name, :email, :password, :first_name, :last_name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:screen_name, :remote_avatar_url, :email, :password, :current_password, :avatar, :bio, :location, :first_name, :last_name])
+ end
 end
