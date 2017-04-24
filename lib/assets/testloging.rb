4b48e40dbf2bc3f6bce44f692f20ad35f46291dd
Jason Jordan <jtjordan13@gmail.com>
2017-04-24 18:30:27 -0400

    Messing with test login to see json error messages

--- lib/assets/testloging.rb
+++ lib/assets/testloging.rb
@@ -5,16 +5,17 @@ resp = HTTParty.post("http://Prompted-db.herokuapp.com/auth",
     :query => { :email => "jtjordan16@gmail.com",
                 :password => "hunter2",
                 :password_confirmation => "hunter2",
+                :screen_name => "Tarqanian",
                 :confirm_success_url => 'http://localhost:3000'})
 
 puts resp.code
 
 binding.pry
 
-resp2 = HTTParty.post("http://localhost:3000/auth/sign_in",
+resp2 = HTTParty.post("http://Prompted-db.herokuapp.com/auth/sign_in",
     :query => { :email => "jtjordan16@gmail.com",
-                :password => "hunter2",
-                :password_confirmation => "hunter2",
+                :password => "hunter3",
+                :password_confirmation => "hunter3",
                 :confirm_success_url => 'http://localhost:3000'})
 
 binding.pry
