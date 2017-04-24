136878d8d167c9be62c84728042dbd75c8d83a1f
Jason Jordan <jtjordan13@gmail.com>
2017-04-24 18:57:56 -0400

    Added defaults to type of and status for prompts

--- db/migrate/20170424223521_create_prompts.rb
+++ db/migrate/20170424223521_create_prompts.rb
@@ -1,9 +1,9 @@
 class CreatePrompts < ActiveRecord::Migration[5.0]
   def change
     create_table :prompts do |t|
-      t.string :type_of
+      t.string :type_of, default: 'unassigned'
       t.text :content
-      t.string :status
+      t.string :status, default: 'upcomming'
       t.string :theme
       t.datetime :due_date
       t.datetime :voting_closed
