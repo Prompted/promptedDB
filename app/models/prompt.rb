3eb7aaf7d9a15bd1af649d6cddb81f1e48b539b1
Jason Jordan <jtjordan13@gmail.com>
2017-05-04 19:52:49 -0400

    Added custom validation for prompts

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -4,4 +4,12 @@ class Prompt < ApplicationRecord
   #validates_presence_of :due_date
   validates_presence_of :status
   #validates_presence_of :voting_closed
+  validate :can_only_be_open_if_duedate_and_votingclosed_are_set
+
+
+  def can_only_be_open_if_duedate_and_votingclosed_are_set
+    if self.status == 'open' && (!self.due_date.present? || !self.voting_closed.present?)
+        errors.add(:cant_open, "Cannot open a prompt for submissions without a due date and voting closed date")
+    end
+  end
 end
