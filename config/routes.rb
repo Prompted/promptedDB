c1563f21f5f1f53ef77245f0ef15b3a7126a3296
Jason Jordan <jtjordan13@gmail.com>
2017-05-04 21:56:33 -0400

    Added Admin homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
 
   root 'home#readme'
   get '/prompts/active' => 'prompts#active'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #routes for admin site
+  get '/admins/admin/:id' => 'admins#show'
+  get '/admins/prompt/new' => 'admins/prompts#new'
+  post '/admins/prompt/create' => 'admins/prompts#create'
+  # post '/admins/show/:tvrage_id' => 'admins/shows#create'
+  # get '/admins/show/:id/edit' => 'admins/shows#edit'
+  # patch '/admins/show/:id/update' => 'admins/shows#update'
+  # patch '/admins/show/:id/deactivate' => 'admins/shows#deactivate'
+  # patch '/admins/show/:id/activate' => 'admins/shows#activate'
+  # get '/admins/shows' => 'admins/shows#index'
 end
